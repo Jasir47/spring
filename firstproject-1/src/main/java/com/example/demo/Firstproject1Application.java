3fcd49b980301b3ba65b56abf575c2bdcda953cc
jasir <muhammedjasirmk01@gmail.com>
2022-07-26 23:28:27 +0530

    second commit added text

--- firstproject-1/src/main/java/com/example/demo/Firstproject1Application.java
+++ firstproject-1/src/main/java/com/example/demo/Firstproject1Application.java
@@ -8,6 +8,7 @@ public class Firstproject1Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Firstproject1Application.class, args);
+		System.out.println("welcome to first application");
 	}
 
 }
