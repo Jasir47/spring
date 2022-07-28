7a3b5c68db4138ac36e802b152a041fc26ec7b45
jasir <muhammedjasirmk01@gmail.com>
2022-07-28 10:50:27 +0530

    Fetching data from  client

--- myWebApp/src/main/java/com/example/demo/homeController.java
+++ myWebApp/src/main/java/com/example/demo/homeController.java
@@ -1,6 +1,9 @@
 package com.example.demo;
 
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -9,8 +12,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class homeController {
     
 	@RequestMapping("home")
-	public String func() {
-		System.out.println("hiii");
+	public String func(HttpServletRequest req) {
+		
+		HttpSession session = req.getSession();
+		
+		String name = req.getParameter("name");
+		System.out.println("hiii "+name);
+		
+		session.setAttribute("name",name);
 		return "home";
 	}
 }
--- myWebApp/src/main/webapp/pages/home.jsp
+++ myWebApp/src/main/webapp/pages/home.jsp
@@ -1,13 +1,14 @@
+
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
 <!DOCTYPE html>
 <html>
 <head>
 <meta charset="UTF-8">
-<title>Insert title here</title>
+<title>myWebApp</title>
 </head>
 <body>
- welcome home
+ welcome ${name}
 </body>
 </html>
 
