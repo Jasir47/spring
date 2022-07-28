d7b2bca69d1b23087f469585e97f3296e600e9b2
jasir <muhammedjasirmk01@gmail.com>
2022-07-28 11:12:28 +0530

    ModelAndView method

--- myWebApp/src/main/java/com/example/demo/homeController.java
+++ myWebApp/src/main/java/com/example/demo/homeController.java
@@ -1,25 +1,23 @@
 package com.example.demo;
 
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 
 @Controller
 public class homeController {
     
 	@RequestMapping("home")
-	public String func(HttpServletRequest req) {
+	public ModelAndView func(@RequestParam("name") String myName) {
+	    
+		ModelAndView mv = new ModelAndView();
+		mv.addObject("name",myName);
+		mv.setViewName("home");
 		
-		HttpSession session = req.getSession();
-		
-		String name = req.getParameter("name");
-		System.out.println("hiii "+name);
-		
-		session.setAttribute("name",name);
-		return "home";
+		return mv;
 	}
 }
