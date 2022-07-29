8b768ab134ce436d2a51a0a3e1de65d5c2bf378a
jasir <muhammedjasirmk01@gmail.com>
2022-07-29 16:48:29 +0530

    Update and Delete mapping

--- bootjpa/src/main/java/com/example/demo/homeController.java
+++ bootjpa/src/main/java/com/example/demo/homeController.java
@@ -7,9 +7,11 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -53,4 +55,23 @@ public class homeController {
 	   return repo.findById(customerId);
 	
    }
+   
+   @SuppressWarnings("deprecation")
+   @DeleteMapping("/customer/{customerId}")
+   public String deleteCustomer(@PathVariable int customerId) {
+	   
+	   customer c = repo.getOne(customerId);
+	   repo.delete(c);
+	   return "deleted";
+       
+   }
+   
+   @PutMapping(path="/customer")
+   public customer updateCustomer(@RequestBody customer customer) {
+	   
+	   repo.save(customer);
+	   return customer ;
+   }
+   
+   
 }
