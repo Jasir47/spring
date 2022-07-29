c7565edde1111ca8484d1a94ac91da2b38731f28
jasir <muhammedjasirmk01@gmail.com>
2022-07-29 11:18:01 +0530

    Mapping post request

--- bootjpa/src/main/java/com/example/demo/homeController.java
+++ bootjpa/src/main/java/com/example/demo/homeController.java
@@ -7,16 +7,20 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.dao.customerRepo;
 import com.example.demo.model.customer;
 
 
 
-@Controller
+@RestController
 public class homeController {
    
    @Autowired
@@ -29,15 +33,14 @@ public class homeController {
    
 	}
 	
-   @RequestMapping("/addCustomer")
-   public String addCustomer(customer customer) {
+   @PostMapping("/customer")
+   public customer addCustomer(@RequestBody customer customer) {
 	   
 	   repo.save(customer);
-	   return "home.jsp";
+	   return customer ;
    }
    
-   @RequestMapping("/customer") 
-   @ResponseBody
+   @GetMapping("/customer") 
    public List<customer> getCustomers() {
 	   
 	   return repo.findAll();
@@ -45,7 +48,6 @@ public class homeController {
    } 
    
    @RequestMapping("/customer/{customerId}") 
-   @ResponseBody
    public Optional<customer> getCustomer(@PathVariable("customerId") int customerId) {
 	   
 	   return repo.findById(customerId);
