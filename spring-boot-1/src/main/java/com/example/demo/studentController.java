83b21b50209606444f29324c35d9d8c133290449
jasir <muhammedjasirmk01@gmail.com>
2022-08-03 14:53:13 +0530

    Path variable and Request parameter

--- spring-boot-1/src/main/java/com/example/demo/studentController.java
+++ spring-boot-1/src/main/java/com/example/demo/studentController.java
@@ -3,6 +3,8 @@ package com.example.demo;
 import java.util.*;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -14,6 +16,8 @@ public class studentController {
 		return Student;
 	}
     
+	
+	// returning a list of students 
 	@GetMapping("/students")
 	public List<student> getStudents(){
 		List<student> students  = new ArrayList<>();
@@ -25,4 +29,22 @@ public class studentController {
 		students.add(new student("great","khali"));
 		return students;
 	}
+	
+	
+    // handling path variable @PathVariable  API s
+	@GetMapping("student/{firstName}/{lastName}")
+	public student studentPV(@PathVariable("firstName") String fN ,
+			@PathVariable ("lastName") String lN) {
+		return new student(fN,lN);
+	}
+	
+	// handling request params using REST API 
+	@GetMapping("/student/query")
+	public student studentRP(@RequestParam(name = "firstName") String fN,
+			@RequestParam(name = "lastName" ) String lN) {
+		return new student(fN,lN);
+	}
+	
+	
+	
 }
