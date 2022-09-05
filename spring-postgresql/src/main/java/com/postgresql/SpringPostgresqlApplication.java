9a6166eed8159daf87ff8152ed4a9feb7280d2d1
jasir <muhammedjasirmk01@gmail.com>
2022-09-05 20:46:30 +0530

    connected to postgresql

---
+++ spring-postgresql/src/main/java/com/postgresql/SpringPostgresqlApplication.java
@@ -0,0 +1,30 @@
+package com.postgresql;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+@SpringBootApplication
+public class SpringPostgresqlApplication implements CommandLineRunner {
+
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
+	
+	
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPostgresqlApplication.class, args);
+	}
+	
+	@Override
+	public void run(String... args) throws Exception {
+	     String sql = "INSERT INTO datas (name,mail) VALUES ("
+	     		+ "'jasir' , 'jasir@mail' )" ;
+	     int rows = jdbcTemplate.update(sql);
+	     if (rows>0) {
+	    	 System.out.println("new row added");
+	     }
+	} 
+
+}
